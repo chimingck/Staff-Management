16e1d84f86cfa77b94ad6860ba2abf12212af273
chimingck <chimingck@gmail.com>
2019-04-24 16:45:45 +0800

    Finished adding & deleting self-defined field function, and modify the display employees function, and added more sample cases

--- Staff_management.cpp
+++ Staff_management.cpp
@@ -27,25 +27,33 @@ void clear_screen()
   cout << string( 100, '\n' );
 }
 
-void display_employee(const vector<Record> &employees, int index)
+void display_employee(const vector<Record> &employees, const vector<string> &self_defined_fields, int index)
 {
   cout << left << setw(5) << to_string(index)+"." << " "
        << left << setw(12) << employees[index].id << " "
        << left << setw(20) << employees[index].name << " "
        << left << setw(3) << employees[index].age << " "
        << left << setw(20) << employees[index].role << " "
-       << left << fixed << setprecision(2) << employees[index].salary << endl;
+       << left << setw(10) << fixed << setprecision(2) << employees[index].salary << " ";
+  // output self-defined field(s)
+  for (vector<string>::size_type i = 0; i != self_defined_fields.size(); ++i)
+    cout << left << setw(self_defined_fields[i].size()+5) << employees[index].self_defined[i] << " ";
+  cout << endl;
 }
 
-void display_all_employees(const vector<Record> &employees)
+void display_all_employees(const vector<Record> &employees, const vector<string> &self_defined_fields)
 {
   // Header, optional
   cout << left << setw(6) << "INDEX" << left << setw(13) << "ID"
        << left << setw(21) << "NAME" << left << setw(4) << "AGE"
-       << left << setw(21) << "ROLE" << "SALARY" << endl;
+       << left << setw(21) << "ROLE" << left << setw(11) << "SALARY";
+  for (auto s : self_defined_fields)
+    cout << left << setw(s.size()+6) << s;
+  cout << endl;
+
   // loop through vector<Record> employees
   for (vector<Record>::size_type i = 0; i != employees.size(); ++i)
-    display_employee(employees, i);
+    display_employee(employees, self_defined_fields, i);
 }
 
 // field
@@ -54,6 +62,7 @@ void display_all_employees(const vector<Record> &employees)
 // 3.Age
 // 4.Role
 // key must be converted string
+// To make it simple, do not support search by self-defined-attribute(s) for now.
 // return a vector<int> which store the index(es) of all employees who matches the key.
 vector<int> search_employee(const vector<Record> &employees, int field, string key)
 {
@@ -136,7 +145,7 @@ int fire_employee(vector<Record> &employees, int index)
 // return -1 if there is no existing employee with specified id.
 // return -2 if the new_value is empty or cannot be converted to int/double properly. i.e. invalid input.
 // return -3 if the field is invalid.
-int modify_employee(vector<Record> &employees, const vector<string> &self_defined_field, int index, int field, string new_value)
+int modify_employee(vector<Record> &employees, const vector<string> &self_defined_fields, int index, int field, string new_value)
 {
   if (index < 0 || index >= employees.size())
     return -1; // Index out of range
@@ -165,7 +174,7 @@ int modify_employee(vector<Record> &employees, const vector<string> &self_define
     else
       return -2; // Invalid input for salary
   }
-  else if (field <= 5+self_defined_field.size()) // between 5 and self_defined_field.size()
+  else if (field <= 5+self_defined_fields.size()) // between 5 and self_defined_field.size()
   {
     (employees[index]).self_defined[field-6] = new_value;
   }
@@ -195,21 +204,67 @@ vector<int> search_by_salary(vector<Record> &employees, double low, double high)
   return results;
 }
 
-void add_self_defined()
+// return 0 if the attriute/field is added and initialized successfully.
+// return -1 if there is an existing self-defined field/attribute with same name. 
+int add_self_defined(vector<Record> &employees, vector<string> &self_defined_fields, string field_name, string default_value = "")
 {
+  for (vector<string>::size_type i = 0; i != self_defined_fields.size(); ++i)
+    if (self_defined_fields[i] == field_name)
+      return -1; // Same field/attribute already exist
   
+  self_defined_fields.push_back(field_name);
+  for (auto &record : employees)
+    record.self_defined.push_back(default_value);
+  return 0;
 }
 
-void delete_self_defined()
+// ***Provided 2 similar functions here. Choose the one that you find it convinient and you can delete the another one.***
+
+// return 0 if the attriute/field is deleted successfully.
+// return -1 if there is no existing self-defined field/attribute with specified name. 
+int delete_self_defined_by_name(vector<Record> &employees, vector<string> &self_defined_fields, string field_name)
 {
-  
+  vector<string>::size_type i = 0;
+  bool found = false;
+  // Not to delete elements of a vector inside the loop of that vector
+  for (i = 0; i != self_defined_fields.size(); ++i)
+    if (self_defined_fields[i] == field_name)
+    {
+      found = true;
+      break;
+    }
+  if (found)
+  {
+    for (auto &record : employees)
+      record.self_defined.erase(record.self_defined.begin()+i);
+    self_defined_fields.erase(self_defined_fields.begin()+i);
+    return 0;
+  }
+  else
+    return -1; // field not found.
+}
+
+// return 0 if the attriute/field is deleted successfully.
+// return -1 if there is no existing self-defined field/attribute with specified index. i.e. index out of range .
+int delete_self_defined_by_index(vector<Record> &employees, vector<string> &self_defined_fields, int index)
+{
+  if (index >= 0 && index < self_defined_fields.size())
+    {
+      for (auto &record : employees)
+        record.self_defined.erase(record.self_defined.begin()+index);
+
+      self_defined_fields.erase(self_defined_fields.begin()+index);
+      return 0;
+    }
+  else
+    return -1; // index out of range.
 }
 
 int main()
 { 
   // Sample Test Case
   vector<Record> employees;
-  vector<string> self_defined_field;
+  vector<string> self_defined_fields;
 
   create_employee(employees, "3035574141", "Alvin", 18, "student", 100);
   create_employee(employees, "3035574142", "Wilson", 19, "student", 200);
@@ -219,7 +274,7 @@ int main()
   create_employee(employees, "0000000000", "Ken", 26, "ceo", 50000);
   create_employee(employees, "9999999999", "Lucy", 43, "accountant", 23000.64);
 
-  display_all_employees(employees);
+  display_all_employees(employees, self_defined_fields);
 
   cout << endl;
   // search by ID
@@ -285,17 +340,41 @@ int main()
   // So when deleting the employee with index 4, is actually deleting the employee with index 5 originally
   // which is Ken, the CEO xd
   fire_employee(employees, 4);
-  display_all_employees(employees);
+  display_all_employees(employees, self_defined_fields);
 
-  cout << endl << "Now modify the role of Alvin from ''student'' to ''ceo''." << endl;
-  modify_employee(employees, self_defined_field, 0, 4, "ceo");
-  display_all_employees(employees);
+  cout << endl << "Now modify the role of Alvin from \"student\" to \"ceo\"." << endl;
+  modify_employee(employees, self_defined_fields, 0, 4, "ceo");
+  display_all_employees(employees, self_defined_fields);
 
   cout << "Role = ceo: ";
   vector<int> roles2 = search_employee(employees, 4, "ceo");
   for (auto i : roles2)
     cout << i << " ";
   cout << endl;
+
+  cout << endl << "Now try to modify field 6 which is not exist for now (the 1st self-defined field)." << endl;
+  cout << "The runction modify_employee will return -3 inidicating non-exist field." << endl;
+  int s = modify_employee(employees, self_defined_fields, 4, 6, "Haaa");
+  cout << s << endl;
+
+  cout << endl << "Now add a self-defined field called \"sex\" and initialize it to \"M\"." << endl;
+  // be aware that the self-defined field is case sensitive.
+  add_self_defined(employees, self_defined_fields, "sex", "M");
+  display_all_employees(employees, self_defined_fields);
+
+  cout << endl << "Now add another self-defined field called \"sport\" and default initialize it to empty." << endl;
+  add_self_defined(employees, self_defined_fields, "sport");
+  display_all_employees(employees, self_defined_fields);
+
+  cout << endl << "Now modify Lucy's \"sport\" field to badminton and \"sex\" to \"F\"." << endl;
+  modify_employee(employees, self_defined_fields, 4, 6, "F");
+  modify_employee(employees, self_defined_fields, 4, 7, "badminton");
+  display_all_employees(employees, self_defined_fields);
+
+  cout << endl << "Now delete the self-defined field \"sex\"." << endl;
+  delete_self_defined_by_name(employees, self_defined_fields, "sex");
+  // delete_self_defined_by_index(employees, self_defined_fields, 0);
+  display_all_employees(employees, self_defined_fields);
 
   /*
   int input_choice = 0;
